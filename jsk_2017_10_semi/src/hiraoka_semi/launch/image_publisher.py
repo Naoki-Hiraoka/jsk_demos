6b3cfd914f7d1eddae17599ee1969b8b0559aa81
Naoki-Hiraoka <hiraokanaoki0330@gmail.com>
2017-11-17 10:14:53 +0900

    171115_画像認識の改良

---
+++ jsk_2017_10_semi/src/hiraoka_semi/launch/image_publisher.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+
+import dynamic_reconfigure.client
+
+def callback(config):
+    rospy.loginfo("sending")
+    
+if __name__ == "__main__":
+    rospy.init_node("dynamic_client")
+    
+    rospy.wait_for_service("/image_publisher/set_parameters")
+    print "service found"
+    client = dynamic_reconfigure.client.Client("/image_publisher", timeout=30, config_callback=callback)
+    print "client"
+   # r = rospy.Rate(0.1)
+  #  x = 0
+ #   b = False
+#    while not rospy.is_shutdown():
+    #    x = x+1
+     #   if x>10:
+      #      x=0
+      #  b = not b
+    client.update_configuration({"file_name":"/home/mech-user/semi_ws/src/jsk_demos/jsk_2017_10_semi/src/hiraoka_semi/image/camera1.jpeg"})
+    #    r.sleep()
